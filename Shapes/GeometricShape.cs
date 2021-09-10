0a7e7ac9255cca47f54f4a89174e0ee63ee4a9d7
Billy Gauldin <bildgaul@ut.utm.edu>
2021-09-10 08:56:44 -0500

    Add project files.

---
+++ Shapes/GeometricShape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    interface GeometricShape
+    {
+        public void Draw()
+        {
+            
+        }
+    }
+}
---
+++ Shapes/ShapeFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    enum ShapeType { Line, Circle, Rectangle, Triangle };
+    class ShapeFactory
+    {
+
+        GeometricShape GetShape(ShapeType type)
+        {
+            return new GeometricShape(type);
+        }
+    }
+}
